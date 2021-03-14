21875cc931bd379c8d947b6dd3ca3a8aa78ca241
Андрей Жданов <cuter.exe@gmail.com>
2021-03-14 18:26:17 +0300

    refactor: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,1 +1,15 @@
-module.exports = function repeater(str, options) {};
+module.exports = function repeater(str, options) {
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  addition = String(addition);
+  const additionalPart = new Array(additionRepeatTimes)
+    .fill(addition)
+    .join(additionSeparator);
+  return new Array(repeatTimes).fill(str + additionalPart).join(separator);
+};
