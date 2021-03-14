c254100e663a18cb77ff06cecf7db17e78e50f6e
Андрей Жданов <cuter.exe@gmail.com>
2021-03-14 19:53:10 +0300

    refactor: implement getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,33 @@
-const CustomError = require("../extensions/custom-error");
+const DATE_ERROR = 'Unable to determine the time of year!';
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const SEASON_WINTER = 'winter';
+const SEASON_SPRING = 'spring';
+const SEASON_SUMMER = 'summer';
+const SEASON_FALL = 'fall';
+
+module.exports = function getSeason(date) {
+  if (!date) return DATE_ERROR;
+  if (Object.keys(date).length > 0) {
+    throw new Error();
+  }
+
+  let month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return SEASON_WINTER;
+    case 2:
+    case 3:
+    case 4:
+      return SEASON_SPRING;
+    case 5:
+    case 6:
+    case 7:
+      return SEASON_SUMMER;
+    case 8:
+    case 9:
+    case 10:
+      return SEASON_FALL;
+  }
 };
