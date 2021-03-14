0a1372b31dc19a3daff608f3a7a6eb94ab25b469
Андрей Жданов <cuter.exe@gmail.com>
2021-03-14 21:02:38 +0300

    refactor: implemetn vigenere-cipher function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,48 @@
-const CustomError = require("../extensions/custom-error");
-
+const CustomError = require('../extensions/custom-error');
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direction = direction;
+  }
+
+  encrypt(message, key) {
+    return this.crypt(message, key, this.ecryptFn);
+  }
+
+  decrypt(message, key) {
+    return this.crypt(message, key, this.decryptFn);
+  }
+
+  crypt(message, key, cryptFn) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+
+    let notMatchedTotal = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[\W\d]/)) {
+        result += message[i];
+        notMatchedTotal++;
+        continue;
+      }
+      const mCode = message.codePointAt(i);
+      const kCode = this.getKCode(i, key, notMatchedTotal);
+      let resCode = cryptFn(mCode, kCode);
+      result += String.fromCodePoint(resCode);
+    }
+    return this.direction ? result : result.split('').reverse().join('');
+  }
+
+  getKCode(i, key, notMatchedTotal) {
+    return i >= key.length
+      ? key.codePointAt((i - notMatchedTotal) % key.length)
+      : key.codePointAt(i - notMatchedTotal);
+  }
+
+  ecryptFn(mCode, kCode) {
+    return ((mCode - 65 + kCode - 65) % 26) + 65;
+  }
+  decryptFn(mCode, kCode) {
+    return ((mCode + 26 - kCode) % 26) + 65;
   }
 }
 
